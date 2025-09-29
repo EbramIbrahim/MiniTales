139742d5ab32b5b299a20c02fb7db9443295900e
alamedahealthcare <abramibrahem22@gmail.com>
2025-09-29 14:06:07 +0300

    add theme module

--- app/src/main/java/com/example/minitales/MainActivity.kt
+++ app/src/main/java/com/example/minitales/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MiniTalesTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Text("Hello Abraam...")
+                    Text("Hello Abraam...", modifier = Modifier.padding(innerPadding))
                 }
             }
         }
